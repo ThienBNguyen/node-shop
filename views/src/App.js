7f19b22afd1eca40b4d585b16f8364d8de489689
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-09-24 22:27:44 -0500

    add react build a form

---
+++ views/src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./App.css";
+
+import axios from 'axios';
+
+export default class ProductForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: ''
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    }
+
+    console.log(user);
+
+    axios.post('http://localhost:5000/products', user)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      username: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New Product</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+export default App;
