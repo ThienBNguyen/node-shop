dc6d0c7b8bead69440a425ea9e9137fb3438aa61
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-09-24 21:42:14 -0500

    initialize set up done with rest api

---
+++ app.js
@@ -0,0 +1,70 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//require path to the end points
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+//use express middlewere to the end point of the path
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+// Connection URL to dot env
+const url = process.env.DATAURI;
+// Database Name
+
+// Use connect method to connect to the server
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  function (err, client) {
+    console.log('Connected successfully to server');
+  }
+);
+
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
